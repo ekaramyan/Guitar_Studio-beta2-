f88551e932eea313c8998a6e11e9c6208c77cfc7
Edos Ddos <56231244+ekaramyan@users.noreply.github.com>
2021-07-07 18:01:55 +0300

    Add files via upload

---
+++ Guitar_Studio-Beta2/Audio(panels).xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Guitar_Studio_Beta2
+{
+    /// <summary>
+    /// Логика взаимодействия для Audio_inout_.xaml
+    /// </summary>
+    public partial class Audio_inout_ : Window
+    {
+        public Audio_inout_()
+        {
+            InitializeComponent();
+        }
+    }
+}
